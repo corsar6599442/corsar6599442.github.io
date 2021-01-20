7c7addf933f3c6a8c4f31c25642e0829a21b132b
Mikhail_Zakharov <corsar6599442@gmail.com>
2021-01-20 23:32:08 +0300

    Добавил приветствие

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 var myHeading = document.querySelector('h1')
 var myImage = document.querySelector('img')
+var myButton = document.querySelector('button')
+var myHeading = document.querySelector('h1')
 
 myHeading.textContent = 'Hello world!'
 
@@ -10,4 +12,17 @@ myImage.onclick = function() {
     } else {
       myImage.setAttribute ('src','images/firefox-icon.png')
     }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = `You're cool dude, ${storedName}`;
+}
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
 }
